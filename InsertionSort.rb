03cf690d56e7213a18f5024423296b0e14db4589
Franklin Antony <franklinanton.007@gmail.com>
2018-01-20 19:16:57 +0530

    Insertion Sort Added

---
+++ InsertionSort.rb
@@ -0,0 +1,19 @@
+def insertion_sort(a)
+    for i in 1...(a.length)
+        j=i
+        while j>0
+            if a[j-1]>a[j]
+                temp=a[j]
+                a[j]=a[j-1]
+                a[j-1]=temp
+            else
+              break
+            end
+            j=j-1
+        end
+    end
+    return a
+end
+
+a=[1,12,3,2,3]
+p insertion_sort(a)
